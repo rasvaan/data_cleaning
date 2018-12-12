b4f17dcab101f09a6ae23cf0d1772e333be71462
Chris Dijkshoorn <c.dijkshoorn@rijksmuseum.nl>
2018-12-12 09:23:43 +0100

    Restructured merge file and added print statements.

--- clean/merge.py
+++ clean/merge.py
@@ -25,50 +25,56 @@ def merge_dat():
     merged_file = 'out/merged.dat'
     # get list of relvant files
     file_paths = list_file_paths(source_folder)
-    # open (all?) files
-    readers = open_files(file_paths)
+    # create dictionary for data
+    data = csv_to_dict(file_paths)
     # create dat file
-    file = codecs.open(merged_file, 'w', 'utf-8')
-    file.write(u'\ufeff')
-    # write each value of each object to new dat
-    write_csv_to_dat(readers, file)
-    # close files
-    file.close()
-    close_readers = close_files(readers)
+    write_dict_to_dat(data, merged_file)
+    print('Finished merge script')
 
-
-def open_files(paths):
-    readers = []
-
-    for path in paths:
-        # extract tag from file name
-        tag = 'stub'
-        file = codecs.open(path, 'r', 'utf-8')
-        readers.append([tag, file])
-    return readers
-
-
-def write_csv_to_dat(readers, file):
-    for reader in readers:
-        tag = reader[0]
-        csv_reader = csv.reader(reader[1], delimiter=',', quotechar='"')
-        for row in csv_reader:
-            value = row[1]
-            print(value)
-            file.write(value)
-
-
-def close_files(readers):
-    for reader in readers:
-        file = reader[1]
-        file.close()
 
 def list_file_paths(source_folder):
+    print('Listing csv files in folder ' + source_folder)
     csv_files = []
 
     for file in glob.glob(source_folder + "/*.csv"):
         csv_files.append(file)
     return csv_files
+
+
+def csv_to_dict(paths):
+    print('Loading csv files into dictionary')
+    dict = {"RM1" : {"IL":"artwork"}}
+
+    for path in paths:
+        file = codecs.open(path, 'r', 'utf-8')
+        # add data to dict
+        # for reader in readers:
+        #     tag = reader[0]
+        #     csv_reader = csv.reader(reader[1], delimiter=',', quotechar='"')
+        #     for row in csv_reader:
+        #         value = row[1]
+        #         print(value)
+        #         file.write(value)
+    return dict
+
+
+def write_dict_to_dat(dict, file):
+    print('Writing dictionary to ' + file)
+    file = codecs.open(file, 'w', 'utf-8')
+    file.write(u'\ufeff')
+    # write each value of each object to new dat
+    # write_dict_to_dat(readers, file)
+    # close files
+    file.close()
+    # close_readers = close_files(readers)
+
+
+
+# def close_files(readers):
+#     for reader in readers:
+#         file = reader[1]
+#         file.close()
+
 
 if __name__ == "__main__":
     merge_dat()
