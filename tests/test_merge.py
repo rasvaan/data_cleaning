0df726a5103909f932aa0cef548d17ef04bb8a1f
Chris Dijkshoorn <c.dijkshoorn@rijksmuseum.nl>
2018-12-12 15:20:26 +0100

    Added round trip test.

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -7,7 +7,8 @@ import os
 import random
 import unittest
 import csv
-from clean import merge
+import filecmp
+from clean import merge, split
 
 
 class TestListCsvFiles(unittest.TestCase):
@@ -25,13 +26,21 @@ class TestMerge(unittest.TestCase):
 
     def tearDown(self):
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        out = os.path.join(data_folder, 'test_merge')
-        contents = os.listdir(out)
-
-        for content in contents:
-            path = os.path.join(out, content)
+        out_merge = os.path.join(data_folder, 'test_merge')
+        contents_merge = os.listdir(out_merge)
+        for content in contents_merge:
+            path = os.path.join(out_merge, content)
             if os.path.isfile(path):
                 os.remove(path)
+
+        out_split = os.path.join(data_folder, 'test_split')
+        contents_split = os.listdir(out_split)
+        for content in contents_split:
+            path = os.path.join(out_split, content)
+            if os.path.isfile(path):
+                os.remove(path)
+
+
 
 
     def test_unicode(self):
@@ -59,6 +68,23 @@ class TestMerge(unittest.TestCase):
                 unicode_present = True
         self.assertEquals(unicode_present, True)
 
+
+    def test_round_trip(self):
+        """ Test if splitting and merging results in the original file. """
+        data_folder = os.path.join(os.getcwd(), 'tests/data')
+        # split files
+        source_file = os.path.join(data_folder, 'source/two_objects.dat')
+        out = os.path.join(data_folder, 'test_split')
+        topics = split.list_topics_dat(source_file, [])
+        split.split_dat_in_csv(topics, source_file, out)
+        # merge file
+        csv_files = merge.list_file_paths(data_folder + '/test_split')
+        merged_file = os.path.join(os.getcwd(), 'tests/data/test_merge/merge-split.dat')
+        data = merge.csv_to_dict(csv_files)
+        merge.write_dict_to_dat(data, merged_file)
+        # test if merged files are the same as original
+        same = filecmp.cmp(source_file, merged_file, shallow=False)
+        self.assertEquals(same, True)
 
 if __name__ == '__main__':
     unittest.main()
