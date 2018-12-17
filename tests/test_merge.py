fc9816099435f5b4d1eba32785cb89d19d649e27
Chris Dijkshoorn <c.dijkshoorn@rijksmuseum.nl>
2018-12-17 10:26:55 +0100

    Refactor tests.

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -17,7 +17,7 @@ class TestListCsvFiles(unittest.TestCase):
     def test_csv(self):
         """ Test listing csv files. """
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        csv_files = merge.list_file_paths(data_folder + '/source')
+        csv_files = merge.list_file_paths(data_folder + '/merge')
         self.assertEquals(len(csv_files), 3)
 
 
@@ -26,29 +26,20 @@ class TestMerge(unittest.TestCase):
 
     def tearDown(self):
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        out_merge = os.path.join(data_folder, 'test_merge')
-        contents_merge = os.listdir(out_merge)
-        for content in contents_merge:
-            path = os.path.join(out_merge, content)
+        output_folder = os.path.join(data_folder, 'out')
+        contents_output_folder = os.listdir(output_folder)
+        for content in contents_output_folder:
+            path = os.path.join(output_folder, content)
             if os.path.isfile(path):
                 os.remove(path)
-
-        out_split = os.path.join(data_folder, 'test_split')
-        contents_split = os.listdir(out_split)
-        for content in contents_split:
-            path = os.path.join(out_split, content)
-            if os.path.isfile(path):
-                os.remove(path)
-
-
 
 
     def test_unicode(self):
         """ Test handling of non-ascii characters. """
         unicode_present = False
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        csv_files = merge.list_file_paths(data_folder + '/source')
-        merged_file = os.path.join(os.getcwd(), 'tests/data/test_merge/merge-test-unicode.dat')
+        csv_files = merge.list_file_paths(data_folder + '/merge')
+        merged_file = os.path.join(os.getcwd(), 'tests/data/out/merge-test-unicode.dat')
         data = merge.csv_to_dict(csv_files)
         merge.write_dict_to_dat(data, merged_file)
         with open(merged_file) as dat_file:
@@ -59,8 +50,8 @@ class TestMerge(unittest.TestCase):
 
     def test_order(self):
         """ Test retaining of order values. """
-        csv_file = os.path.join(os.getcwd(), 'tests/data/source/1_%0.csv')
-        merged_file = os.path.join(os.getcwd(), 'tests/data/test_merge/merge.dat')
+        csv_file = os.path.join(os.getcwd(), 'tests/data/merge/1_%0.csv')
+        merged_file = os.path.join(os.getcwd(), 'tests/data/out/merge.dat')
         data_dict = merge.csv_to_dict([csv_file])
         merge.write_dict_to_dat(data_dict, merged_file)
         with open(merged_file) as dat_file:
@@ -73,13 +64,13 @@ class TestMerge(unittest.TestCase):
         """ Test if splitting and merging results in the original file. """
         data_folder = os.path.join(os.getcwd(), 'tests/data')
         # split files
-        source_file = os.path.join(data_folder, 'source/two_objects.dat')
-        out = os.path.join(data_folder, 'test_split')
+        source_file = os.path.join(data_folder, 'split/two_objects.dat')
+        out = os.path.join(data_folder, 'out')
         topics = split.list_topics_dat(source_file, [])
         split.split_dat_in_csv(topics, source_file, out)
         # merge file
-        csv_files = merge.list_file_paths(data_folder + '/test_split')
-        merged_file = os.path.join(os.getcwd(), 'tests/data/test_merge/merge-split.dat')
+        csv_files = merge.list_file_paths(out)
+        merged_file = os.path.join(os.getcwd(), 'tests/data/out/merge-split.dat')
         data = merge.csv_to_dict(csv_files)
         merge.write_dict_to_dat(data, merged_file)
         # test if merged files are the same as original
--- tests/test_split.py
+++ tests/test_split.py
@@ -16,7 +16,7 @@ class TestListTopics(unittest.TestCase):
     def test_topics_one(self):
         """ Test listing topics from one record. """
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        source = os.path.join(data_folder, 'source/one_object.dat')
+        source = os.path.join(data_folder, 'split/one_object.dat')
         ignore = ['IN', 'I3']
         topics1 = split.list_topics_dat(source, ignore)
         topics2 = set([u'%0', u'BA', u'BE', u'MP', u'mP'])
@@ -25,7 +25,7 @@ class TestListTopics(unittest.TestCase):
     def test_topics_two(self):
         """ Test listing topics from two records. """
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        source = os.path.join(data_folder, 'source/two_objects.dat')
+        source = os.path.join(data_folder, 'split/two_objects.dat')
         topics1 = split.list_topics_dat(source, [])
         topics2 = set([u'%0', u'BA', u'BE'])
         self.assertEquals(topics1, topics2)
@@ -33,7 +33,7 @@ class TestListTopics(unittest.TestCase):
     def test_topics_break(self):
         """ Test listing topics from file with line break. """
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        source = os.path.join(data_folder, 'source/line_break.dat')
+        source = os.path.join(data_folder, 'split/line_break.dat')
         topics1 = split.list_topics_dat(source, [])
         topics2 = set([u'%0', u'BA', u'TF'])
         self.assertEquals(topics1, topics2)
@@ -43,7 +43,7 @@ class TestOutputSplits(unittest.TestCase):
 
     def tearDown(self):
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        out = os.path.join(data_folder, 'test_split')
+        out = os.path.join(data_folder, 'out')
         contents = os.listdir(out)
 
         for content in contents:
@@ -55,8 +55,8 @@ class TestOutputSplits(unittest.TestCase):
         """ Test csv creation. """
         topics = set([u'%0', u'BA', u'BE', u'MP', u'mP'])
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        source = os.path.join(data_folder, 'source/one_object.dat')
-        out = os.path.join(data_folder, 'test_split')
+        source = os.path.join(data_folder, 'split/one_object.dat')
+        out = os.path.join(data_folder, 'out')
         split.split_dat_in_csv(topics, source, out)
         files1 = os.listdir(out)
         files2 = ['1_%0.csv', '2_BA.csv', '3_BE.csv', '4_MP.csv', '5_mP.csv']
@@ -67,8 +67,8 @@ class TestOutputSplits(unittest.TestCase):
         """ Test outputting csv file of one record and topic. """
         topic = u'BE'
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        source = os.path.join(data_folder, 'source/one_object.dat')
-        out = os.path.join(data_folder, 'test_split')
+        source = os.path.join(data_folder, 'split/one_object.dat')
+        out = os.path.join(data_folder, 'out')
         split_file = os.path.join(out, topic + '.csv')
         file = open(split_file, 'w')
         writer = csv.writer(file)
@@ -81,8 +81,8 @@ class TestOutputSplits(unittest.TestCase):
         """ Test outputting csv file of two records and one topic. """
         topic = u'BE'
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        source = os.path.join(data_folder, 'source/two_objects.dat')
-        out = os.path.join(data_folder, 'test_split')
+        source = os.path.join(data_folder, 'split/two_objects.dat')
+        out = os.path.join(data_folder, 'out')
         split_file = os.path.join(out, topic + '.csv')
         file = open(split_file, 'w')
         writer = csv.writer(file)
@@ -95,8 +95,8 @@ class TestOutputSplits(unittest.TestCase):
         """ Test outputting csv file of records with value containing line break. """
         topic = u'TF'
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        source = os.path.join(data_folder, 'source/line_break.dat')
-        out = os.path.join(data_folder, 'test_split')
+        source = os.path.join(data_folder, 'split/line_break.dat')
+        out = os.path.join(data_folder, 'out')
         split_file = os.path.join(out, topic + '.csv')
         file = open(split_file, 'w')
         writer = csv.writer(file)
@@ -109,8 +109,8 @@ class TestOutputSplits(unittest.TestCase):
         """ Test outputting csv file of records with value containing line break, but focussing on other value. """
         topic = u'BA'
         data_folder = os.path.join(os.getcwd(), 'tests/data')
-        source = os.path.join(data_folder, 'source/line_break.dat')
-        out = os.path.join(data_folder, 'test_split')
+        source = os.path.join(data_folder, 'split/line_break.dat')
+        out = os.path.join(data_folder, 'out')
         split_file = os.path.join(out, topic + '.csv')
         file = open(split_file, 'w')
         writer = csv.writer(file)
